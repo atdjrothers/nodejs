e26191a84cf66d7d97900117ec3772960d249092
atdjrothers <atdjrothers@gmail.com>
2021-07-30 15:42:00 +0800

    attendance forgotten endpoints

--- FinalExam/controllers/attendanceController.js
+++ FinalExam/controllers/attendanceController.js
@@ -11,6 +11,36 @@ const Joi = require('joi').extend(require('@joi/date'));
 
 
 /**
+ * Gets all attendance
+ * @param {Request} req
+ * @param {Response} res
+ * @param {NextFunction} next
+ */
+ const getAllAttendance = async (req, res, next) => {
+	LOGGER.logHttpRequest(req, 'getAllAttendance');
+	const output = await attendanceDataAccess.getAll();
+	res.send(output);
+};
+
+/**
+ * Get attendance by id.
+ * @param {Request} req
+ * @param {Response} res
+ * @param {NextFunction} next
+ */
+ const getAttendanceById = async (req, res, next) => {
+	LOGGER.logHttpRequest(req, 'getAttendanceById');
+	const data = await attendanceDataAccess.getById(req.params.id);
+	if (data) {
+		res.send(data);
+	} else {
+		next(ERROR_HANDLER.createErrorResponse(APP_CONSTANTS.ERROR_MESSAGE.EVENT_RECORD_NOT_FOUND, 404, APP_CONSTANTS.ERROR_TYPES.RECORD_NOT_FOUND));
+	}
+};
+
+
+
+/**
  * Validate if Attendancename or Email Address already exists.
  * @param {Request} req
  * @param {Response} res
@@ -22,7 +52,7 @@ const validateCreateRequest = async (req, res, next) => {
 	const schema = Joi.object({
 		eventId: Joi.string().required(),
 		memberId: Joi.string().required(),
-		timeIn: Joi.date().format(APP_CONSTANTS.DATETIME_FORMAT).utc(),
+		timeIn: Joi.date().format(APP_CONSTANTS.DATETIME_FORMAT).utc().less(Joi.ref('timeOut')).required(),
 		timeOut: Joi.date().format(APP_CONSTANTS.DATETIME_FORMAT).utc()
 	});
 
@@ -63,6 +93,63 @@ const insertAttendance = async (req, res, next) => {
 
 
 /**
+ * Validate Update Request Payload
+ * @param {Request} req
+ * @param {Response} res
+ * @param {NextFunction} next
+ */
+ const validateUpdateRequest = async (req, res, next) => {
+	LOGGER.logHttpRequest(req, 'updateAttendance');
+	const payload = req.body;
+	const schema = Joi.object({
+		id: Joi.string().required(),
+		eventId: Joi.string().required(),
+		memberId: Joi.string().required(),
+		name: Joi.string(),
+		timeIn: Joi.date().format(APP_CONSTANTS.DATETIME_FORMAT).utc().less(Joi.ref('timeOut')).required(),
+		timeOut: Joi.date().format(APP_CONSTANTS.DATETIME_FORMAT).utc()
+	});
+
+	const { error } = schema.validate(payload);
+	if (error) {
+		next(ERROR_HANDLER.createErrorResponse(error.message, 400, APP_CONSTANTS.ERROR_TYPES.VALIDATION_ERROR));
+	}
+
+	const attendance = await attendanceDataAccess.getById(payload.id);
+	if (!attendance) {
+		next(ERROR_HANDLER.createErrorResponse(APP_CONSTANTS.ERROR_MESSAGE.ATTENDANCE_RECORD_NOT_FOUND, 404, APP_CONSTANTS.ERROR_TYPES.RECORD_NOT_FOUND));
+	}
+
+	const member = await memberDataAccess.getById(payload.memberId);
+	if (!member) {
+		next(ERROR_HANDLER.createErrorResponse(APP_CONSTANTS.ERROR_MESSAGE.MEMBER_RECORD_NOT_FOUND, 400, APP_CONSTANTS.ERROR_TYPES.INTEGRITY_CONSTRAINT));
+	} else {
+		req.body.name = member.name;
+	}
+
+	const event = await eventDataAccess.getById(payload.eventId);
+	if (!event) {
+		next(ERROR_HANDLER.createErrorResponse(APP_CONSTANTS.ERROR_MESSAGE.EVENT_RECORD_NOT_FOUND, 400, APP_CONSTANTS.ERROR_TYPES.INTEGRITY_CONSTRAINT));
+	}
+
+	next();
+};
+
+
+/**
+ * Update attendance.
+ * @param {Request} req
+ * @param {Response} res
+ * @param {NextFunction} next
+ */
+const updateAttendance = async (req, res, next) => {
+	const payload = req.body;
+	await attendanceDataAccess.update(payload);
+	res.sendStatus(200);
+};
+
+
+/**
  * Delete attendance.
  * @param {Request} req
  * @param {Response} res
@@ -82,7 +169,11 @@ const deleteAttendance = async (req, res, next) => {
 
 
 module.exports = {
+	getAllAttendance,
+	getAttendanceById,
 	validateCreateRequest,
 	insertAttendance,
+	validateUpdateRequest,
+	updateAttendance,
 	deleteAttendance
 };
--- FinalExam/routers/attendanceRouter.js
+++ FinalExam/routers/attendanceRouter.js
@@ -3,9 +3,17 @@ const { attendanceController } = require('../controllers');
 
 const router = express.Router();
 
+router.get('/', attendanceController.getAllAttendance);
+router.get('/:id', attendanceController.getAttendanceById);
+
 router.post('/',
     attendanceController.validateCreateRequest,
     attendanceController.insertAttendance
+);
+
+router.put('/',
+    attendanceController.validateUpdateRequest,
+    attendanceController.updateAttendance
 );
 
 router.delete('/:id', attendanceController.deleteAttendance);
